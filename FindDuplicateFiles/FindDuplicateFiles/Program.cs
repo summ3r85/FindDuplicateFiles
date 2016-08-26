9f224479b2c13e1042feb8972dabe837c56d17d1
Summ3R <summ3r85@gmail.com>
2016-08-26 21:17:01 +0300

    added args validation and exeption when getting DirectoryInfo

--- FindDuplicateFiles/FindDuplicateFiles/Program.cs
+++ FindDuplicateFiles/FindDuplicateFiles/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Net.Mime;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+
 
 namespace FindDuplicateFiles
 {
     class Program
-    {   
+    {
         public class FileKeys
         {
 
@@ -23,13 +17,12 @@ namespace FindDuplicateFiles
                 return "filename: " + FileObj.Name + "   Size: " + FileObj.Length;
             }
 
-            
+
         }
+
         static bool CompareTwoFiles(FileKeys first, FileKeys second)
         {
-            if (first.FileObj.Length != second.FileObj.Length)
-                return false;
-
+            
             using (FileStream fs1 = first.FileObj.OpenRead())
             using (FileStream fs2 = second.FileObj.OpenRead())
             {
@@ -46,7 +39,7 @@ namespace FindDuplicateFiles
         static List<FileKeys> DeleteUniqueItems(List<FileKeys> fileLists)
         {
             List<FileKeys> tempFileLists = new List<FileKeys>();
-           while (fileLists.Count > 0)
+            while (fileLists.Count > 0)
             {
                 int i = 0;
                 bool isDuplicated = false;
@@ -74,55 +67,72 @@ namespace FindDuplicateFiles
         static void Main(string[] args)
         {
 
-            string directoryArg = args[0];
-            if (directoryArg.Length<0)
+         
+            if (args.Length==0)
             {
-                Console.WriteLine(@"Directory is not entered. Usage: FindDuplicateFiles.exe C:\Dir");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(@"Directory is not entered.");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(@"FindDuplicateFiles.exe DirectoryPath. Example: FindDuplicateFiles.exe C:\Dir");
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.ReadLine();
+                Environment.Exit(0);
+
+            }
+            string directoryArg = args[0];
+            Console.WriteLine("Scanning Folder {0}", directoryArg);
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(directoryArg);
+                FileInfo[] fileList = di.GetFiles();
+                List<FileKeys> fl = new List<FileKeys>();
+
+                foreach (FileInfo filesInfo in fileList)
+                {
+                    fl.Add(new FileKeys() { FileObj = filesInfo });
+                }
+
+                List<FileKeys> flsorted = DeleteUniqueItems(fl);
+
+                //Sort by Size;
+                flsorted.Sort((keys, fileKeys) => (int)keys.FileObj.Length);
+
+                while (flsorted.Count > 0)
+                {
+                    int i = 0;
+                    int jj = 0;
+                    for (int j = 1; j < flsorted.Count; j++)
+                    {
+                        if (flsorted[i].FileObj.Length == flsorted[j].FileObj.Length)
+                        {
+                            if (CompareTwoFiles(flsorted[i], flsorted[j]))
+                            {
+                                Console.WriteLine("{0}\r\n{1}\r\n", flsorted[i], flsorted[j]);
+                            }
+                        }
+                        else
+                        {
+                            flsorted.RemoveAt(i);
+                            break;
+                        }
+                        jj = j;
+                    }
+                    if (jj == flsorted.Count - 1)
+                    {
+                        flsorted.RemoveAt(0);
+                    }
+
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:{0}", e);    
                 
             }
-            Console.WriteLine("Scanning Folder {0}", directoryArg);
-            DirectoryInfo di = new DirectoryInfo(directoryArg);
-
-            FileInfo[] fileList = di.GetFiles();
-            List<FileKeys> fl = new List<FileKeys>();
             
-            foreach (FileInfo filesInfo in fileList)
-            {
-                fl.Add(new FileKeys() {FileObj = filesInfo} );
-            }
 
-            List<FileKeys> flsorted = DeleteUniqueItems(fl);
-
-            //Sort by Size;
-            flsorted.Sort((keys, fileKeys) =>(int)keys.FileObj.Length);
-
-            while (flsorted.Count>0)
-            {
-                int i = 0;
-                int jj = 0;
-                for (int j = 1; j <flsorted.Count; j++)
-                {
-                    if (flsorted[i].FileObj.Length == flsorted[j].FileObj.Length)
-                    {
-                        if (CompareTwoFiles(flsorted[i], flsorted[j])) { 
-                            Console.WriteLine("{0}\r\n{1}\r\n", flsorted[i], flsorted[j]);
-                            }
-                    }
-                    else
-                    {
-                        flsorted.RemoveAt(i);
-                        break;
-                    }
-                    jj = j;
-                }
-                if (jj==flsorted.Count-1)
-                {
-                    flsorted.RemoveAt(0);
-                }
-
-            }
-            Console.ReadLine();
+            
         }
     }
 }
